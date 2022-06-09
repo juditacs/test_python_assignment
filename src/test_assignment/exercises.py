c92f2e91c9548fb8f1db3a86bb57d63ad9d2ee2a
Judit Acs <judit@sch.bme.hu>
2022-06-09 17:05:13 +0200

    Add incomplete task

--- src/test_assignment/exercises.py
+++ src/test_assignment/exercises.py
@@ -1,10 +1,20 @@
-from typing import Any, Sequence
+from typing import Any, Dict, Sequence
 
 
-# Define a function that takes a sequence as its input and returns whether the sequence is symmetric.
-# A sequence is symmetric if it is equal to its reverse.
+# Define a function that takes a sequence as its input and returns whether the
+# sequence is symmetric. A sequence is symmetric if it is equal to its reverse.
+
 def is_symmetric(sequence: Sequence[Any]) -> bool:
     for i in range(len(sequence) // 2):
         if sequence[i] != sequence[-(i+1)]:
             return False
     return True
+
+
+# Define a function that takes a sequence and counts the frequency of each
+# element in the list. The function should return a dictionary of the counts.
+# Do not use the collections module.
+
+def count_elements(sequence: Sequence[Any]) -> Dict[Any, int]:
+    # TODO
+    pass
--- tests/test_exercises.py
+++ tests/test_exercises.py
@@ -1,6 +1,6 @@
 import pytest
 
-from test_assignment.exercises import is_symmetric
+from test_assignment.exercises import is_symmetric, count_elements
 
 
 @pytest.mark.parametrize(
@@ -12,8 +12,23 @@ from test_assignment.exercises import is_symmetric
         (["abc", "bcd"], False),
         (["abc", "bcd","abc"], True),
         ((1, 2, 2, 1), True),
+        ([None, None], True),
+        ([1, 2, 3, 1], False),
+        ([1, "foo", "bar", "foo", 1], True),
+        (list(range(100)) + list(range(99, -1, -1)), True),
+        (list(range(100)) + list(range(100, 0, -1)), False),
+        ([1, "1"], False),
     ],
 )
 def test_is_symmetric(sequence, answer):
     assert is_symmetric(sequence) == answer
 
+
+def test_count_elements():
+    assert count_elements([1, 2, 0, 1, 1]) == {1: 3, 2: 1, 0: 1}
+    # test for tuple
+    assert count_elements((1, 2, 0, 1, 1)) == {1: 3, 2: 1, 0: 1}
+    # test empty list
+    assert count_elements([]) == {}
+    # test mixed types
+    assert count_elements([1, "1", 1]) == {1: 2, "1": 1}
